1c24869a1bf5a9f0c930074b1e333328e5ce8c55
admin <admin@10.211.55.3>
2017-03-21 20:13:04 +0800

    sql

--- src/main/resources/SQL/wuya.sql
+++ src/main/resources/SQL/wuya.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50554
 File Encoding         : 65001
 
-Date: 2017-03-21 18:34:36
+Date: 2017-03-21 20:12:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,6 +27,32 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Records of admin
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for secret_question
+-- ----------------------------
+DROP TABLE IF EXISTS `secret_question`;
+CREATE TABLE `secret_question` (
+  `questionId` varchar(255) NOT NULL DEFAULT '0' COMMENT '密保问题id 随机产生',
+  `uid` varchar(255) NOT NULL DEFAULT '' COMMENT '用户id',
+  `questionInfo` varchar(255) DEFAULT '' COMMENT '问题详情',
+  `answerInfo` varchar(255) DEFAULT '' COMMENT '密保问题答案',
+  `status` int(2) DEFAULT '0' COMMENT '删除状态',
+  PRIMARY KEY (`questionId`,`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of secret_question
+-- ----------------------------
+INSERT INTO `secret_question` VALUES ('1', '1', '1', '1', null);
+INSERT INTO `secret_question` VALUES ('2', '2', '2', '2', null);
+INSERT INTO `secret_question` VALUES ('3', '1', '3', '3', null);
+INSERT INTO `secret_question` VALUES ('4', '1', '学号？', '2013142202', null);
+INSERT INTO `secret_question` VALUES ('tt', 'tt', '测试', '无题', null);
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -34,6 +60,7 @@ CREATE TABLE `user` (
   `uid` varchar(255) NOT NULL DEFAULT '' COMMENT '用户唯一id',
   `loginName` varchar(255) NOT NULL DEFAULT '' COMMENT '用户登录名',
   `pwd` varchar(255) NOT NULL DEFAULT '' COMMENT '用户密码',
+  `bind_email` varchar(255) DEFAULT '' COMMENT '绑定邮箱账号',
   `nickName` varchar(255) DEFAULT '' COMMENT '用户昵称',
   `sex` int(2) DEFAULT '1' COMMENT '性别 默认1->男 0->女',
   `signature` varchar(255) DEFAULT '' COMMENT '个性签名',
@@ -46,3 +73,7 @@ CREATE TABLE `user` (
   `profile` varchar(255) DEFAULT '' COMMENT '用户个人简介',
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
