a98654bf2af587cc0ccf228e350f454b5e4b0e05
Cyy <Cyy@Cinyky>
2017-03-21 23:15:44 +0800

    sql

--- src/main/resources/SQL/wuya.sql
+++ src/main/resources/SQL/wuya.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
+Source Server         : local
 Source Server Version : 50554
 Source Host           : localhost:3306
 Source Database       : wuya
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50554
 File Encoding         : 65001
 
-Date: 2017-03-21 20:12:25
+Date: 2017-03-21 23:14:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -61,16 +61,17 @@ CREATE TABLE `user` (
   `loginName` varchar(255) NOT NULL DEFAULT '' COMMENT '用户登录名',
   `pwd` varchar(255) NOT NULL DEFAULT '' COMMENT '用户密码',
   `bind_email` varchar(255) DEFAULT '' COMMENT '绑定邮箱账号',
+  `email_code` varchar(255) DEFAULT '' COMMENT '邮箱验证码存放',
   `nickName` varchar(255) DEFAULT '' COMMENT '用户昵称',
   `sex` int(2) DEFAULT '1' COMMENT '性别 默认1->男 0->女',
   `signature` varchar(255) DEFAULT '' COMMENT '个性签名',
-  `birth` bigint(255) DEFAULT '0' COMMENT '生日',
+  `profile` varchar(255) DEFAULT '' COMMENT '用户个人简介',
   `location` varchar(255) DEFAULT '' COMMENT '居住地',
   `headPic` varchar(255) DEFAULT '' COMMENT '头像',
+  `birth` bigint(255) DEFAULT '0' COMMENT '生日',
   `regTime` bigint(255) DEFAULT NULL COMMENT '注册时间',
-  `status` int(2) DEFAULT '-1' COMMENT '用户账号状态 -1未激活 0 正常 1封号',
   `banTime` bigint(255) DEFAULT '0' COMMENT '封号时间点',
-  `profile` varchar(255) DEFAULT '' COMMENT '用户个人简介',
+  `status` int(2) DEFAULT '-1' COMMENT '用户账号状态 -1未激活 0 正常 1封号',
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
