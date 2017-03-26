a1e073789d2f9fa1f75a7c8ca0c7cb8ddf7d2500
Cyy <Cyy@Cinyky>
2017-03-26 22:57:03 +0800

    friend dao

---
+++ src/main/java/com/wuya/cyy/dao/FriendDao.java
@@ -0,0 +1,45 @@
+package com.wuya.cyy.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.wuya.cyy.pojo.Focus;
+import com.wuya.cyy.pojo.Friend;
+
+
+/**
+ * 好友dao
+ * Cinyky 
+ *
+ * 2017年3月26日下午9:39:03
+ */
+public interface FriendDao {
+	/**
+	 * 增加friend
+	 * @param friend
+	 * @return
+	 */
+	int addFriend(Friend friend);
+	
+	/**
+	 * 更新friend
+	 * @param friend
+	 * @return
+	 */
+	int updateFriend(Friend friend);
+	
+	
+	/**
+	 * 查询friend 根据uid anotherUid
+	 * @return
+	 */
+	List<Friend> selectFriendByUid(String uid);
+	
+	/**
+	 * 查询friend 根据uid anotherUid
+	 * @return
+	 */
+	List<Friend> selectFriendByAnotherUid(String anotherUid);
+	
+}
