bf07a5f99959a2a0e788afd4f52cab26309006b5
cyy <cyy@cyy-MS-7850>
2017-03-30 20:21:45 +0800

    interceptor

--- src/main/java/com/wuya/cyy/filter/UserFilter.java
+++ src/main/java/com/wuya/cyy/filter/UserFilter.java
@@ -12,6 +12,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
@@ -21,66 +23,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
  * 30 Mar 2017 17:44:23
  */
 public class UserFilter extends	OncePerRequestFilter{
-
-//	@Override
-//	public void init(FilterConfig filterConfig) throws ServletException {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//			throws IOException, ServletException {
-//		// TODO Auto-generated method stub
-//	       // 请求的uri
-//		HttpServletRequest httpServletRequest = (HttpServletRequest)request;
-//		HttpServletRequest httpServletResponse = (HttpServletRequest)response;
-//	    String uri = httpServletRequest.getRequestURI();
-//		String[] notFilter = new String[] { "login", "reg" };
-//	 
-//	            boolean doFilter = true;
-//	            for (String s : notFilter) {
-//	                if (uri.indexOf(s) != -1) {
-//	                    // 如果uri中包含不过滤的uri，则不进行过滤
-//	                    doFilter = false;
-//	                    break;
-//	                }
-//	            }
-//	            if (doFilter) {
-//	                // 执行过滤
-//	                // 从session中获取登录者实体
-//	                Object obj = httpServletRequest.getSession().getAttribute("user");
-//	                if (null == obj) {
-//	                    // 如果session中不存在登录者实体，则弹出框提示重新登录
-//	                    // 设置request和response的字符集，防止乱码
-//	                    request.setCharacterEncoding("UTF-8");
-//	                    response.setCharacterEncoding("UTF-8");
-//	                    PrintWriter out = response.getWriter();
-//	                    String loginPage = "http://localhost:8080/wuya/";
-//	                    StringBuilder builder = new StringBuilder();
-//	                    builder.append("<script type=\"text/javascript\">");
-//	                    builder.append("alert('网页过期，请重新登录！');");
-//	                    builder.append("window.top.location.href='");
-//	                    builder.append(loginPage);
-//	                    builder.append("';");
-//	                    builder.append("</script>");
-//	                    out.print(builder.toString());
-//	                } else {
-//	                    // 如果session中存在登录者实体，则继续
-//	                	chain.doFilter(request, response);
-//	                }
-//	            } else {
-//	                // 如果不执行过滤，则继续
-//	            	chain.doFilter(request, response);
-//	            }
-//	}
-//
-//	@Override
-//	public void destroy() {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
--- src/main/java/com/wuya/cyy/interceptor/LoginInterceptor.java
+++ src/main/java/com/wuya/cyy/interceptor/LoginInterceptor.java
@@ -5,11 +5,13 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class LoginInterceptor implements HandlerInterceptor{
-
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
